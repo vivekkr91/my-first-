0c4f16c5c0a3d9c32262fda1f403b2a870229dea
vivekkr91 <vivek888gaya@gmail.com>
2024-10-30 19:02:47 +0530

    array file added

---
+++ array.js
@@ -0,0 +1,7 @@
+//array in js
+let fruits = ['mango','apple','orange'];
+let codinglanguage = ['python','c','c++'];
+console.log(fruits);
+console.log(codinglanguage);
+console.log(fruits[0]);
+console.log(codinglanguage[1]);
